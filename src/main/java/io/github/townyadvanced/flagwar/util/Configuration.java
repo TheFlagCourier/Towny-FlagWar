c354c68901a908af7caf16e9b0bd676827088ca3
FlagCourier <flagcourier@pm.me>
2020-12-16 15:09:42 -0600

    Basis for Config keys. Establishes keys and default values. Currently targets Toml, but could do Yaml or Json, too. Might need to swap getOrDefault() out with getOrSetDefault()... Something to look into more detail. Not sure if the null path will place the file at this point in time...
    Also, need to implement setting default flagBaseBlock, flagLightMaterial, and beaconWireFrameMaterial.
    
    Once things are confirmed working, need to remove legacy commented code.

--- src/main/java/io/github/townyadvanced/flagwar/util/Configuration.java
+++ src/main/java/io/github/townyadvanced/flagwar/util/Configuration.java
@@ -3,10 +3,13 @@ package io.github.townyadvanced.flagwar.util;
 import com.palmergames.bukkit.config.ConfigNodes;
 import com.palmergames.bukkit.towny.TownySettings;
 import com.palmergames.util.TimeTools;
+import de.leonhard.storage.Toml;
 import org.bukkit.Material;
 import org.bukkit.Tag;
 
 public class Configuration {
+
+	private static final Toml conf = new Toml("config.toml","");
 
 	protected static final Material[] woolColors = new Material[] {
 			Material.LIME_WOOL, Material.GREEN_WOOL, Material.BLUE_WOOL, Material.CYAN_WOOL,
@@ -18,7 +21,7 @@ public class Configuration {
 	private static Material beaconWireFrameMaterial = null;
 
 	private Configuration() {
-		// Utility class, mask implicit public constructor
+		// Utility class, mask implicit public constructor.
 	}
 
 	public static boolean isAffectedMaterial(Material material) {
@@ -32,23 +35,28 @@ public class Configuration {
 	}
 
 	public static boolean isAllowingAttacks() {
-
-		return TownySettings.getBoolean(ConfigNodes.WAR_ENEMY_ALLOW_ATTACKS);
+		// Default to false for testing purposes.
+		return conf.getOrDefault("allow-attacks", false);
+		//return TownySettings.getBoolean(ConfigNodes.WAR_ENEMY_ALLOW_ATTACKS);
 	}
 
 	public static long getFlagWaitingTime() {
-
-		return TimeTools.convertToTicks(TownySettings.getSeconds(ConfigNodes.WAR_ENEMY_FLAG_WAITING_TIME));
+		// Default to 50000L for testing
+		// TODO: Port TimeTools? Keep using it? Or use a different (agnostic) library?
+		return TimeTools.convertToTicks(conf.getOrDefault("flag.waiting-time", 50000L));
+		//return TimeTools.convertToTicks(TownySettings.getSeconds(ConfigNodes.WAR_ENEMY_FLAG_WAITING_TIME));
 	}
 
 	public static boolean isDrawingBeacon() {
 
-		return TownySettings.getBoolean(ConfigNodes.WAR_ENEMY_BEACON_DRAW);
+		return conf.getOrDefault("beacon.draw", true);
+		//return TownySettings.getBoolean(ConfigNodes.WAR_ENEMY_BEACON_DRAW);
 	}
 
 	public static int getMaxActiveFlagsPerPerson() {
 
-		return TownySettings.getInt(ConfigNodes.WAR_ENEMY_MAX_ACTIVE_FLAGS_PER_PLAYER);
+		return conf.getOrDefault("rule.max-active-flags-per-player", 1);
+		//return TownySettings.getInt(ConfigNodes.WAR_ENEMY_MAX_ACTIVE_FLAGS_PER_PLAYER);
 	}
 
 	public static Material getFlagBaseMaterial() {
@@ -68,7 +76,8 @@ public class Configuration {
 
 	public static int getBeaconRadius() {
 
-		return TownySettings.getInt(ConfigNodes.WAR_ENEMY_BEACON_RADIUS);
+		return conf.getOrDefault("beacon.radius", 3);
+		//return TownySettings.getInt(ConfigNodes.WAR_ENEMY_BEACON_RADIUS);
 	}
 
 	public static int getBeaconSize() {
@@ -78,24 +87,29 @@ public class Configuration {
 
 	public static int getBeaconMinHeightAboveFlag() {
 
-		return TownySettings.getInt(ConfigNodes.WAR_ENEMY_BEACON_HEIGHT_ABOVE_FLAG_MIN);
+		return conf.getOrDefault("beacon.height-above-flag.min", 3);
+		//return TownySettings.getInt(ConfigNodes.WAR_ENEMY_BEACON_HEIGHT_ABOVE_FLAG_MIN);
 	}
 
 	public static int getBeaconMaxHeightAboveFlag() {
 
-		return TownySettings.getInt(ConfigNodes.WAR_ENEMY_BEACON_HEIGHT_ABOVE_FLAG_MAX);
+		return conf.getOrDefault("beacon.height-above-flag.max", 64);
+		//return TownySettings.getInt(ConfigNodes.WAR_ENEMY_BEACON_HEIGHT_ABOVE_FLAG_MAX);
 	}
 
+	//TODO IMPORT THIS CALL!!!
 	public static void setFlagBaseMaterial(Material flagBaseMaterial) {
 
 		Configuration.flagBaseMaterial = flagBaseMaterial;
 	}
 
+	//TODO IMPORT THIS CALL!!!
 	public static void setFlagLightMaterial(Material flagLightMaterial) {
 
 		Configuration.flagLightMaterial = flagLightMaterial;
 	}
 
+	//TODO IMPORT THIS CALL!!!
 	public static void setBeaconWireFrameMaterial(Material beaconWireFrameMaterial) {
 
 		Configuration.beaconWireFrameMaterial = beaconWireFrameMaterial;
@@ -103,40 +117,49 @@ public class Configuration {
 
 	public static int getMinPlayersOnlineInTownForWar() {
 
-		return TownySettings.getInt(ConfigNodes.WAR_ENEMY_MIN_PLAYERS_ONLINE_IN_TOWN);
+		return conf.getOrDefault("rule.min-players-online.town", 4);
+		//return TownySettings.getInt(ConfigNodes.WAR_ENEMY_MIN_PLAYERS_ONLINE_IN_TOWN);
 	}
 
 	public static int getMinPlayersOnlineInNationForWar() {
 
-		return TownySettings.getInt(ConfigNodes.WAR_ENEMY_MIN_PLAYERS_ONLINE_IN_NATION);
+		return conf.getOrDefault("rule.min-players-online.nation", 3);
+		//return TownySettings.getInt(ConfigNodes.WAR_ENEMY_MIN_PLAYERS_ONLINE_IN_NATION);
 	}
 
 	public static double getWonTownblockReward() {
 
-		return TownySettings.getDouble(ConfigNodes.WAR_ECONOMY_TOWNBLOCK_WON);
+		return conf.getOrDefault("economy.won.town-block", 10.00);
+		//return TownySettings.getDouble(ConfigNodes.WAR_ECONOMY_TOWNBLOCK_WON);
 	}
 
 	public static double getWonHomeblockReward() {
 
-		return TownySettings.getDouble(ConfigNodes.WAR_ECONOMY_HOMEBLOCK_WON);
+		return conf.getOrDefault("economy.won.home-block", 100.00);
+		//return TownySettings.getDouble(ConfigNodes.WAR_ECONOMY_HOMEBLOCK_WON);
 	}
 
 	public static double getCostToPlaceWarFlag() {
 
-		return TownySettings.getDouble(ConfigNodes.WAR_ECONOMY_ENEMY_PLACE_FLAG);
+		return conf.getOrDefault("economy.flag.attack", 10.00);
+		//return TownySettings.getDouble(ConfigNodes.WAR_ECONOMY_ENEMY_PLACE_FLAG);
 	}
 
 	public static double getDefendedAttackReward() {
 
-		return TownySettings.getDouble(ConfigNodes.WAR_ECONOMY_ENEMY_DEFENDED_ATTACK);
+		return conf.getOrDefault("economy.flag.defend", 10.00);
+		//return TownySettings.getDouble(ConfigNodes.WAR_ECONOMY_ENEMY_DEFENDED_ATTACK);
 	}
 
     public static boolean isAttackingBordersOnly() {
 
-        return TownySettings.getBoolean(ConfigNodes.WAR_ENEMY_ONLY_ATTACK_BORDER);
+        return conf.getOrDefault("rule.attack.borders-only", true);
+		//return TownySettings.getBoolean(ConfigNodes.WAR_ENEMY_ONLY_ATTACK_BORDER);
     }
 
 	public static boolean isFlaggedTownblockTransfered() {
-		return TownySettings.getBoolean(ConfigNodes.WAR_ENEMY_FLAG_TAKES_OWNERSHIP_OF_TOWNBLOCKS);
+
+		return conf.getOrDefault("rule.attack.take-ownership", true);
+		//return TownySettings.getBoolean(ConfigNodes.WAR_ENEMY_FLAG_TAKES_OWNERSHIP_OF_TOWNBLOCKS);
 	}
 }
